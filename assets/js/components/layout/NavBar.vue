5ad56b9fc9c894b3d6aaf0bcc767dab065b01a77
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2019-02-17 01:11:38 -0500

    fix navbar

--- assets/js/components/layout/NavBar.vue
+++ assets/js/components/layout/NavBar.vue
@@ -11,27 +11,26 @@
           <span></span>
         </span>
       </div>
-      <div :class="{'is-active': openMenu}" id="navbarMenu" class="navbar-menu">
+      <div :class="{'is-active': openMenu}" id="navbarMenu" class="navbar-menu ">
         <div class="navbar-end">
-          <div class="tabs is-right">
-            <ul>
-              <li :class="{'is-active': activeRoute == '/'}"><a href='/'>Home</a></li>
-              <li v-if="!user" :class="{'is-active': activeRoute == '/login'}"><a href="/login">Login</a></li>
-              <li v-if="!user" :class="{'is-active': activeRoute == '/signup'}"><a href="/signup">Sign Up</a></li>
-              <li v-if="user" :class="{'is-active': activeRoute == '/logout'}"><a href="/logout">Logout</a></li>
-            </ul>
-            <span class="navbar-item">
-              <a class="button is-primary is-outlined" href="https://github.com/danielbarbarito/mojolicious-hackathon-starter">
-                <span class="icon">
-                  <i class="fab fa-github"></i>
-                </span>
-                <span title="Hello from the other side">View Source</span>
-              </a>
-            </span>
-            <span v-if="user" class="navbar-item">
-              Hi, {{user.email}}
-            </span>
+
+          <a class="navbar-item" href='/'>Home</a>
+          <a v-if="!user" :class="{'is-active': activeRoute == '/login'}" class="navbar-item" href='/'>Login</a>
+          <a v-if="!user" :class="{'is-active': activeRoute == '/signup'}" class="navbar-item" href='/'>Sign Up</a>
+          <div style="align-items: center" v-if="user" class="navbar-item has-dropdown">
+           <b-dropdown>
+              <a slot="trigger" class="navbar-link" href="#">Profile</a>
+              <b-dropdown-item><a href="/logout">Logout</a></b-dropdown-item>
+            </b-dropdown>
           </div>
+          <span class="navbar-item">
+            <a class="button is-primary is-outlined" href="https://github.com/danielbarbarito/mojolicious-hackathon-starter">
+              <span class="icon">
+                <i class="fab fa-github"></i>
+              </span>
+              <span title="Hello from the other side">View Source</span>
+            </a>
+          </span>
         </div>
       </div>
     </div>
