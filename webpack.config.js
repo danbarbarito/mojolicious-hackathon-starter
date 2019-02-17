80d4141679ab80a2e3d7be8b78856c2651de9403
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2019-02-17 18:56:21 -0500

    fix asset path

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ const config = {
         {
           loader: 'file-loader',
           options: {
-            publicPath: 'asset'
+            publicPath: '/asset'
           }
         }
       ]
