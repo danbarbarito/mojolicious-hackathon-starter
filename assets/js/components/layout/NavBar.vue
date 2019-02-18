b13fe06c4b7f3d643f85eb67746a963d671653d9
Dan Barbarito <danielbarbarito@mail.adelphi.edu>
2019-02-17 23:46:51 -0500

    better navbar

--- assets/js/components/layout/NavBar.vue
+++ assets/js/components/layout/NavBar.vue
@@ -1,39 +1,61 @@
 <template>
-  <nav class="navbar">
-    <div class="container">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="../">
-          <img src="../../../img/logo.png" />
-        </a>
-        <span :class="{'is-active': openMenu}" @click="openMenu = !openMenu" class="navbar-burger burger" data-target="navbarMenu">
-          <span></span>
-          <span></span>
-          <span></span>
-        </span>
-      </div>
-      <div :class="{'is-active': openMenu}" id="navbarMenu" class="navbar-menu ">
-        <div class="navbar-end">
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <img src="../../../img/logo.png" />
+      </a>
 
-          <a :class="{'is-active': activeRoute == '/'}" class="navbar-item" href='/'>Home</a>
-          <a v-if="!user" :class="{'is-active': activeRoute == '/login'}" class="navbar-item" href='/login'>Login</a>
-          <a v-if="!user" :class="{'is-active': activeRoute == '/signup'}" class="navbar-item" href='/signup'>Sign Up</a>
-          <div style="align-items: center" v-if="user" class="navbar-item has-dropdown">
-           <b-dropdown>
-              <a slot="trigger" class="navbar-link" href="#">Profile</a>
-              <b-dropdown-item><a href="/logout">Logout</a></b-dropdown-item>
-            </b-dropdown>
+      <a :class="{'is-active': openMenu}" @click="openMenu = !openMenu" role="button" class="navbar-burger burger"
+        aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div :class="{'is-active': openMenu}" id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+
+        <!-- <a :class="{'is-active': activeRoute == '/'}" class="navbar-item">
+          Home
+        </a> -->
+
+      </div>
+
+      <div class="navbar-end">
+        <div v-if="!user" :class="{'is-active': activeRoute == '/'}" class="navbar-item">
+          <div class="buttons">
+            <a :class="{'is-active': activeRoute == '/signup'}" href="/signup" class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a :class="{'is-active': activeRoute == '/login'}" href="/login" class="button is-light">
+              Log in
+            </a>
           </div>
-          <span class="navbar-item">
+        </div>
+        <div v-if="user" class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            Profile
+          </a>
+
+          <div class="navbar-dropdown">
+            <a href="/logout" class="navbar-item">
+              Logout
+            </a>
+          </div>
+        </div>
+
+        <div class="navbar-item">
             <a class="button is-primary is-outlined" href="https://github.com/danielbarbarito/mojolicious-hackathon-starter">
               <span class="icon">
                 <i class="fab fa-github"></i>
               </span>
               <span title="Hello from the other side">View Source</span>
             </a>
-          </span>
         </div>
       </div>
     </div>
+
   </nav>
 </template>
 
